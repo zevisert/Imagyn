153c7bde4ae28cb36941ffb73da26badf1320f40
Zev Isert <zev.isert@gmail.com>
2017-07-11 16:46:14 -0700

    Merged multidownloader into imagyn module
    Adds request checking for various failure conditions

--- project/imagyn/collection/download.py
+++ project/imagyn/collection/download.py
@@ -3,85 +3,185 @@ Contains classes and functions regarding downloading the images.
 """
 
 import requests
-import urllib.request
+import multiprocessing
+
 import os
-import lexicon
+import io
+import uuid
+import shutil
+import random
+
+from itertools import repeat
+from PIL import Image, ImageChops
+
+
+from imagyn.collection import lexicon
+from imagyn.collection.utils import binary_images
 
 class Download:
     def __init__(self):
         """
         Contains various image download functions.
         """
-        
         self.lexicon = lexicon.SynsetLexicon()
 
-    def download_images(self, count, image_type, sid, retrieved = 0):
+    def download_sequential(self, urls, destination, prefix):
         """
-        Download a select number of images based on an image type and synset id.
+        Download images in sequentially in one process.
+        :param urls: list of urls to download
+        :param destination: Location to store downloaded images
+        :param prefix: synset id or descriptor word for urls
+        :return: Count of images downloaded successfully
         """
 
-        urls = self.get_synset_urls(sid)
+        retrieved = 0
 
-        # Error offset handles the case when a 404 occurs, which then skips the current url
-        error_offset = 0
-        local_retrieved = 0
-        downloaded_files = []
+        # Loop over the images to retrieve
+        for url in urls:
+            # Retrieve the url at the next index
+            success = self.download_single_checked(
+                url=url,
+                destination=destination,
+                prefix=prefix
+            )
+            if success:
+                retrieved += 1
 
-        file_name = ""
-
-        # Loop until the desired number of images are retrieved
-        while (local_retrieved < count):
-            while True:
-                try:
-                    # Need to fix this with the os.filepath.join(...)
-                    file_name = ".\\CollectedImages\\{}\\img{}.jpg".format(image_type, local_retrieved + retrieved)
-
-                    # Retrieve the url at the next index
-                    urllib.request.urlretrieve(urls[local_retrieved + error_offset], file_name)
-                    local_retrieved += 1
-
-                    break
-                    
-                # Stop trying to receive images if at the end of the list
-                except IndexError:
-                    break
-
-                # Skip to next URL if current image no longer exists
-                except (urllib.error.HTTPError, urllib.error.URLError):
-                    error_offset += 1
-            
-            downloaded_files.append(file_name)
-
-        return downloaded_files
+        return retrieved
     
-    def download_images_multiple_synsets(self, count, image_type, synsets):
+    def download_multiple_synsets(self, count, synsets, destination, sequential=False):
         """
         Download images from multiple synsets.
+        :param count: Number of images to attempt to sample from each synset
+        :param synsets: List of synsets to request images from
+        :param destination: Location to store the downloaded files
+        :param sequential: Use sequential (single threaded) download method
+        :return: Dict of counts of images downloaded from each synset
         """
 
-        images_retrieved = 0
-        downloaded_files = []
+        downloaded_files = dict()
+        downloader = self.download_sequential if sequential else self.multidownload
 
         # If there are a very small number of images to be retrieved then just use first synset
-        calculated_count = (int)(count / len(synsets))
-        if (calculated_count == 0):
+        calculated_count = count // len(synsets)
+        if calculated_count == 0:
             sid = self.lexicon.get_synset_id(synsets[0])
-            downloaded_files.extend(self.download_images(count, image_type, sid, images_retrieved))
+
+            prefix = ""
+            name_req = requests.get(self.lexicon.API['wordsfor'].format(sid))
+            if name_req.status_code == 200:
+                prefix = name_req.content.decode().splitlines()[0].replace(" ", "_")
+
+            downloaded_files[sid] = downloader(
+                urls=random.sample(self.get_synset_urls(sid), count),
+                destination=destination,
+                prefix=prefix
+            )
+
+        # otherwise go through each synset and try to get the number of images required
         else:
             for synset in synsets:
                 sid = self.lexicon.get_synset_id(synset)
-                downloaded_files.extend(self.download_images(calculated_count, image_type, sid, images_retrieved))
-                images_retrieved += calculated_count
+
+                prefix = ""
+                name_req = requests.get(self.lexicon.API['wordsfor'].format(sid))
+                if name_req.status_code == 200:
+                    prefix = name_req.content.decode().splitlines()[0].replace(" ", "_")
+
+                downloaded_files[sid] = downloader(
+                    urls=random.sample(self.get_synset_urls(sid), count),
+                    destination=destination,
+                    prefix=prefix
+                )
 
         return downloaded_files
 
     def get_synset_urls(self, sid):
         """
         Gets the URLs for a corresponding synset id.
+        :param sid:
+        :return:
         """
 
+        urls = []
+        if not self.lexicon.valid_synset(sid):
+            return urls
+
         request = requests.get(self.lexicon.API['urlsfor'].format(sid))
-        urls = request.content.decode().splitlines()
-        del request
+
+        if request.status_code == 200:
+            urls.extend(request.content.decode().splitlines())
 
         return urls
+
+    def multidownload(self, urls: list, destination: str, prefix: str):
+        """
+        Use several processes to speed up image acquisition.
+        :param urls: list of urls to attempt to download
+        :param destination: folder to put images in
+        :param prefix: synset id or descriptor word of urls
+        :return: Count of images downloaded successfully
+        """
+        if not os.path.exists(destination):
+            os.mkdir(destination)
+
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+            # Returns number of images downloaded
+            return sum(pool.starmap(self.download_single_checked, zip(urls, repeat(destination), repeat(prefix))))
+
+    def download_single_checked(self, url: str, destination: str, prefix: str):
+        """
+        Download a single image, checking for failure cases.
+        :param url: Url to attempt to download an image from
+        :param destination: folder to store downloaded image in
+        :param prefix: synset id or descriptor word for url
+        :return: boolean as success if downloaded succeeded
+        """
+        # splits to (`url+filename`, `.`, `filesuffix`)
+        filetype = url.strip().rpartition('.')[2]
+        keep = None
+
+        try:
+            # require either .png, .jpg, or .jpeg
+            if filetype in ['png', 'jpg', 'jpeg']:
+                # We need a naming scheme that won't overwrite anything
+                # Option a) pass in the index with the url
+                # Option b) use a sufficiently sized random number
+                #   > Only after generating 1 billion UUIDs every second for the next 100 years,
+                #   > the prob of creating just one duplicate would be about 50%.
+                #   > The prob of one duplicate would be about 50% if every person on earth owns 600 million UUIDs.
+                file = os.path.join(destination, '{}-{}.{}'.format(prefix, uuid.uuid4(), filetype))
+
+                # Get the file
+                response = requests.get(url, stream=True, timeout=5)
+                if response.status_code == 200:
+                    with open(file, 'wb') as out_file:
+                        response.raw.decode_content = True
+                        shutil.copyfileobj(response.raw, out_file)
+                        keep = False  # None -> False :: We have a file now, need to verify
+
+                    # Check we got an image not some HTML junk 404
+                    with Image.open(file) as img:
+                        # Logic here is that if we can interpret the image then its good
+                        # PIL is lazy - the raster data isn't loaded until needed or `load` is called explicitly'
+                        keep = True  # False -> True :: We've decided to keep the download
+
+                        # Look through the known 'not available images'
+                        for bin_image in binary_images.values():
+
+                            # If this image size matches
+                            if img.size == bin_image['size']:
+
+                                # Compare the raster data
+                                with Image.open(io.BytesIO(bin_image['raster'])) as raster:
+                                    if ImageChops.difference(raster, img).getbbox() is None:
+                                        # No bounding box for the difference of these images, so
+                                        # this is a 'image not availble' image
+                                        keep = False  # True -> False :: Changed our mind..
+
+        # No except - don't care about exceptions
+        # Only care if we've come to the conclusion to keep the file or not
+        finally:
+            if keep is not None and not keep:
+                os.remove(file)
+            return bool(keep)  # Cast None to False
