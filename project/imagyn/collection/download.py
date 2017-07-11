e4d1bf52bee7e883d5e3d072139328c3c9ffb9d4
Matthew James Hodgson <matthodg@uvic.ca>
2017-07-11 15:27:45 -0700

    Added the synset lexicon and download modules.

--- project/imagyn/collection/download.py
+++ project/imagyn/collection/download.py
@@ -1,1 +1,87 @@
-# Build download functions or classes here
+"""
+Contains classes and functions regarding downloading the images.
+"""
+
+import requests
+import urllib.request
+import os
+import lexicon
+
+class Download:
+    def __init__(self):
+        """
+        Contains various image download functions.
+        """
+        
+        self.lexicon = lexicon.SynsetLexicon()
+
+    def download_images(self, count, image_type, sid, retrieved = 0):
+        """
+        Download a select number of images based on an image type and synset id.
+        """
+
+        urls = self.get_synset_urls(sid)
+
+        # Error offset handles the case when a 404 occurs, which then skips the current url
+        error_offset = 0
+        local_retrieved = 0
+        downloaded_files = []
+
+        file_name = ""
+
+        # Loop until the desired number of images are retrieved
+        while (local_retrieved < count):
+            while True:
+                try:
+                    # Need to fix this with the os.filepath.join(...)
+                    file_name = ".\\CollectedImages\\{}\\img{}.jpg".format(image_type, local_retrieved + retrieved)
+
+                    # Retrieve the url at the next index
+                    urllib.request.urlretrieve(urls[local_retrieved + error_offset], file_name)
+                    local_retrieved += 1
+
+                    break
+                    
+                # Stop trying to receive images if at the end of the list
+                except IndexError:
+                    break
+
+                # Skip to next URL if current image no longer exists
+                except (urllib.error.HTTPError, urllib.error.URLError):
+                    error_offset += 1
+            
+            downloaded_files.append(file_name)
+
+        return downloaded_files
+    
+    def download_images_multiple_synsets(self, count, image_type, synsets):
+        """
+        Download images from multiple synsets.
+        """
+
+        images_retrieved = 0
+        downloaded_files = []
+
+        # If there are a very small number of images to be retrieved then just use first synset
+        calculated_count = (int)(count / len(synsets))
+        if (calculated_count == 0):
+            sid = self.lexicon.get_synset_id(synsets[0])
+            downloaded_files.extend(self.download_images(count, image_type, sid, images_retrieved))
+        else:
+            for synset in synsets:
+                sid = self.lexicon.get_synset_id(synset)
+                downloaded_files.extend(self.download_images(calculated_count, image_type, sid, images_retrieved))
+                images_retrieved += calculated_count
+
+        return downloaded_files
+
+    def get_synset_urls(self, sid):
+        """
+        Gets the URLs for a corresponding synset id.
+        """
+
+        request = requests.get(self.lexicon.API['urlsfor'].format(sid))
+        urls = request.content.decode().splitlines()
+        del request
+
+        return urls
--- project/imagyn/collection/lexicon.py
+++ project/imagyn/collection/lexicon.py
@@ -1,1 +1,125 @@
-# Build synset modeling functions or classes here, fetch synsets, check wordnet for availibility etc
+"""
+Contains classes and functions regarding wordnet and imagenet synsets.
+"""
+
+import requests
+import random
+
+from nltk import download
+from nltk.corpus import wordnet as wn
+
+class SynsetLexicon:
+    def __init__(self):
+        """
+        Contains various synset related functions.
+        """
+
+        download("wordnet")
+        self.API = self.get_api()
+        self.synsets = requests.get(self.API['allsynsets']).content.decode().splitlines()
+    
+    def get_api(self):
+        """
+        Gets the API as a dictionary of wordnet and imagenet.
+        """
+
+        API = {
+            'allsynsets': "http://image-net.org/api/text/imagenet.synset.obtain_synset_list",
+            'wordsfor': "http://image-net.org/api/text/wordnet.synset.getwords?wnid={}",
+            'urlsfor': "http://image-net.org/api/text/imagenet.synset.geturls?wnid={}",
+            'hyponymfor': "http://image-net.org/api/text/wordnet.structure.hyponym?wnid={}",
+        }
+        return API
+
+    def get_synset(self, keyword):
+        """
+        Get the synset that matches the given keyword.
+        """
+
+        synset = wn.synset("{}.n.01".format(keyword)) 
+        if (self.valid_synset(synset)):
+            return synset
+        else:
+            # Invalid synset, it is not in WordNet.
+            # Throw exception?
+            pass
+    
+    def get_synset_id(self, synset):
+        """
+        Get the corresponding synset id of the synset.
+        """
+
+        sid = "n{}".format(str(synset.offset()).zfill(8))
+        return sid
+
+    def valid_synset(self, synset):
+        """
+        Determines if the synset is valid by checking to see that it is in ImageNet.
+        """
+
+        sid = self.get_synset_id(synset)
+        inImageNet = sid in self.synsets
+        return inImageNet
+
+    def get_siblings(self, synset):
+        """
+        Returns up to five siblings of the synset.
+        """
+
+        siblings = []
+        siblingCount = 0
+        parent = self.get_parent(synset)
+        for sibling in parent.hyponyms():
+            if siblingCount == 5:
+                break
+            if sibling != synset and self.valid_synset(sibling):
+                siblings.insert(siblingCount, sibling)
+                siblingCount += 1
+        return siblings
+
+    def get_parent(self, synset):
+        """
+        Returns one of the parents of the synset.
+        """
+
+        return random.choice(synset.hypernyms())
+
+    def get_grandparents(self, synset):
+        """
+        Returns all grandparents of the synset.
+        """
+
+        grandparents = []
+        for parent in synset.hypernyms():
+            grandparents.extend(parent.hypernyms())
+        return grandparents
+
+    def get_unrelated_synsets(self, synset):
+        """
+        Gets five unrelated synsets.
+        """
+
+        # Get the matching grandparents in order to ensure unrelated synsets
+        matchGrandparents = self.get_grandparents(synset)
+
+        unrelatedSynsets = []
+        unrelatedCount = 0
+        while (unrelatedCount < 5):
+            while True:
+                try:
+                    unrelatedSynsetId = random.choice(self.synsets)
+                    unrelatedSynsetName = random.choice(requests.get(self.API["wordsfor"].format(unrelatedSynsetId)).content.decode().splitlines())
+                    unrelatedSynset = wn.synset("{}.n.01".format(unrelatedSynsetName))
+                    
+                    # Get grandparents of random synset
+                    unrelatedGrandparents = self.get_grandparents(unrelatedSynset)
+                        
+                    # Ensure valid synset and that it is truely unrelated
+                    if (self.valid_synset(unrelatedSynset) and not bool(set(matchGrandparents) & set(unrelatedGrandparents))):
+                        unrelatedSynsets.insert(unrelatedCount, unrelatedSynset)
+                        unrelatedCount += 1
+                        break
+                except:
+                    print ("{} is not a noun, try again.".format(unrelatedSynsetName))
+
+        return unrelatedSynsets
