c764010885a3a1eccec1dca895fbc8d9151f1fb8
Maxwell A. Borden <maxwellborden@gmail.com>
2017-07-23 13:31:41 -0700

    added Request component for the page after submission

---
+++ project/flask/front-end/src/components/Request.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="request">
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+import axios from 'axios'
+export default {
+  name: 'search',
+  data () {
+    return {
+      query: '',
+      options: []
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.request {
+  display: flex;
+  justify-content: space-around;
+}
+
+h1 {
+  font-size: 10em;
+  margin-bottom: 20px;
+  margin-top: 0;
+}
+
+.request-inner {
+  text-align: center;
+  margin-top: 100px;
+}
+</style>
