7a7d2746d2ae86534bcaaea5706fb4af7201d393
Zev Isert <zev.isert@gmail.com>
2017-07-26 16:28:54 -0700

    Whitespace alterations
    Removed trailing spaces and wrapped RunAll requirements

--- project/luigi/pipeline.py
+++ project/luigi/pipeline.py
@@ -12,18 +12,22 @@ Wrapper task that initiates the other tasks
 
 class RunAll(luigi.WrapperTask):
     keyword = luigi.Parameter()
-    imgCount = luigi.IntParameter() 
+    imgCount = luigi.IntParameter()
     exact = luigi.IntParameter()
     unrelated = luigi.IntParameter()
     similar = luigi.IntParameter()
     st = datetime.datetime.fromtimestamp(time.time()).strftime('%m-%d-%Y_%H_%M_%S')
 
     def requires(self):
-        return [SynthesizeTask(download_type = "Exact", keyword=self.keyword, imgCount=self.imgCount, num_images=self.exact, time=self.st), SynthesizeTask(download_type = "Similar", keyword=self.keyword, imgCount=self.imgCount, num_images=self.similar, time=self.st), SynthesizeTask(download_type = "Unrelated", keyword=self.keyword, imgCount=self.imgCount, num_images=self.unrelated, time=self.st)]
+        return [
+            SynthesizeTask(download_type="Exact",     num_images=self.exact,     keyword=self.keyword, imgCount=self.imgCount, time=self.st),
+            SynthesizeTask(download_type="Similar",   num_images=self.similar,   keyword=self.keyword, imgCount=self.imgCount, time=self.st),
+            SynthesizeTask(download_type="Unrelated", num_images=self.unrelated, keyword=self.keyword, imgCount=self.imgCount, time=self.st)
+        ]
 
     def output(self):
-        return luigi.LocalTarget("output{}.txt".format(self.st)) 
- 
+        return luigi.LocalTarget("output{}.txt".format(self.st))
+
     def run(self):
         with self.output().open('w') as f:
             f.write("done")
@@ -31,16 +35,16 @@ class RunAll(luigi.WrapperTask):
 class DownloadImagesTask(luigi.Task):
     download_type = luigi.Parameter()
     keyword = luigi.Parameter()
-    imgCount = luigi.IntParameter() 
+    imgCount = luigi.IntParameter()
     num_images = luigi.IntParameter()
     time = luigi.Parameter()
 
     def requires(self):
         return []
- 
+
     def output(self):
         return luigi.LocalTarget("{}{}.txt".format(self.download_type, self.time))
- 
+
     def run(self):
         downloader = download. Downloader()
         synset_helper = lexicon.SynsetLexicon()
@@ -64,25 +68,25 @@ class DownloadImagesTask(luigi.Task):
             # Get unrelated images
             synsets.extend(synset_helper.get_unrelated_synsets(synset))
             downloaded_result = downloader.download_multiple_synsets(self.num_images, synsets, path)
-            
+
         with self.output().open('w') as fout:
             for key in downloaded_result:
                 for f in downloaded_result[key]:
                     fout.write(f + "\n")
-                    
+
 class SynthesizeTask(luigi.Task):
     keyword = luigi.Parameter()
-    imgCount = luigi.IntParameter() 
+    imgCount = luigi.IntParameter()
     num_images = luigi.IntParameter()
     time = luigi.Parameter()
     download_type = luigi.Parameter()
 
     def requires(self):
         return [DownloadImagesTask(download_type=self.download_type, keyword=self.keyword, imgCount=self.imgCount, num_images=self.num_images, time=self.time)]
- 
+
     def output(self):
         return luigi.LocalTarget("synthesize_{}{}.txt".format(self.download_type, self.time))
- 
+
     def run(self):
         with self.input()[0].open('r') as f:
             for line in f:
