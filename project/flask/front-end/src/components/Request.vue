e95d6a6803df359619484a373d6c21cbcdc5c790
Maxwell Borden <maxwellborden@gmail.com>
2017-07-27 12:56:41 -0700

    demo latest

--- project/flask/front-end/src/components/Request.vue
+++ project/flask/front-end/src/components/Request.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="request">
-    <div id="cloud-img-wrapper">
-      <img id="cloud-img" src="../assets/cloud.png">
+    <div id="img-wrapper" :class="{ tform : transform_stage}">
       <template v-if="!errored">
-      <template v-if="download_complete">
-        <h3>
-          <i-count-up :start="0" :end="num_images" :duration="3.5"></i-count-up> images retrieved from <span class="bold-italics">the cloud</span>.
-        </h3>
+        <template v-if="transform_stage">
+          <h1>Synthesizing Images!</h1>
+          <img id="img-img" :class="current_animation" src="../assets/toucan-polaroid.jpg">
+        </template>
+        <template v-else>
+          <img id="cloud-img" src="../assets/cloud.png">
+          <template v-if="download_complete">
+            <h3>
+              <i-count-up :start="0" :end="num_images" :duration="3.5"></i-count-up> images retrieved from <span class="bold-italics">the cloud</span>.
+            </h3>
+          </template>
+          <template v-else>
+            <h3>
+              Downloading images from <span class="bold-italics">the cloud</span><span class="dots">{{ dots }}</span>
+            </h3>
+          </template>
+        </template>
       </template>
       <template v-else>
         <h3>
-          downloading images from <span class="bold-italics">the cloud</span><span class="dots">{{ dots }}</span>
-        </h3>
-      </template>
-      </template>
-      <template v-else>
-        <h3>
-          Download Error!
+          Error!
         </h3>
       </template>
     </div>
@@ -52,14 +58,13 @@ export default {
       }
     }, 1000)
     this.$nextTick(function () {
-      console.log('requesting ' + this.query)
       axios.get('http://localhost:5000/api/download/' + this.query)
       .then(response => {
-        console.log('got ' + this.query)
         clearInterval(dotInc)
         this.download_complete = true
         this.num_images = response.data.num_images
         this.tmp_dir = response.data.dirname
+        setTimeout(this.doTransforms(), 3000)
       })
       .catch(() => {
         clearInterval(dotInc)
@@ -75,10 +80,33 @@ export default {
       dot_ticker: 0,
       dots: '.',
       errored: false,
+      current_animation: 'ani_r',
+      transform_stage: false,
       download_complete: false
     }
   },
   methods: {
+    doTransforms: function () {
+      this.transform_stage = true
+      this.dot_ticker = 0
+      setInterval(() => {
+        this.dot_ticker++
+        switch (this.dot_ticker % 3) {
+          case 0:
+            this.current_animation = 'ani_r'
+            break
+          case 1:
+            this.current_animation = 'ani_f'
+            break
+          case 2:
+            this.current_animation = 'ani_s'
+            break
+          default:
+            this.current_animation = 'ani_r'
+            break
+        }
+      }, 2500)
+    }
   }
 }
 </script>
@@ -90,25 +118,70 @@ export default {
   justify-content: space-around;
 }
 
-#cloud-img-wrapper {
+#img-wrapper {
  height: 350px;
  width: 503px;
- padding-top: 50px;
+ padding-top: 350px;
  position: relative;
+ text-align: center;
+}
+
+#img-wrapper.tform {
+  padding-top: 250px;
 }
 
 #cloud-img {
   height: 300px;
 }
 
+#img-img {
+  margin-top: 40px;
+  height: 400px;
+  animation-duration: 2s;
+  animation-delay: 0.5s;
+  animation-timing-function: ease-in-out;
+  border: 1.5px solid gray;
+}
+
 h3 {
   position: absolute;
-  top: 245px;
+  top: 545px;
   text-align: center;
   left: 0;
   margin: 0 auto;
   width: 100%;
 }
+
+.ani_r {
+  animation: rotate;
+}
+
+.ani_s {
+  animation: stretch;
+}
+
+.ani_f {
+  animation: flip;
+}
+
+@keyframes rotate {
+  0% {transform: rotate(0deg)}
+  100% {transform: rotate(360deg)}
+}
+
+@keyframes flip {
+  0% {transform: rotateY(0deg)}
+  50% {transform: rotateY(180deg)}
+  100% {transform: rotateY(0deg)}
+}
+
+@keyframes stretch {
+  0% {transform: scale(1,1)}
+  25% {transform: scale(0.5, 1)}
+  75% {transform: scale(1, 0.5)}
+  100% {transform: scale(1,1)}
+}
+
 .dots {
   width: 32px;
   text-align: left;
--- project/flask/server.py
+++ project/flask/server.py
@@ -2,11 +2,13 @@ import sys
 import os
 sys.path.insert(0, '..')
 import json
+import tarfile
 from tempfile import TemporaryDirectory
 from flask import Flask, Response, jsonify
 from valid_words import word_list
 from imagyn.collection.lexicon import SynsetLexicon #get wnid
 from imagyn.collection.download import Downloader #get images
+from imagyn.synthesis.synthesizer import Synthesizer #transfom stuff
 app = Flask(__name__)
 
 """
@@ -14,11 +16,12 @@ init imagyn stuff
 """
 lex = SynsetLexicon()
 dwn = Downloader()
+synth = Synthesizer()
 
 """
 list of references to temporary directories
 """
-
+tdbase = '\\'.join(TemporaryDirectory().name.split('\\')[:-1])
 tempdirs = {}
 
 """
@@ -40,7 +43,6 @@ downloads images to temporary directory
 @app.route('/api/download/<searchterm>')
 def get_temp_key(searchterm):
     td = TemporaryDirectory()
-    print("Temporary Directory is: " + td.name)
     tempdirs[td.name] = td
     sco = lex.get_synset(searchterm)
     wnid = lex.get_synset_id(sco)
@@ -52,10 +54,24 @@ def get_temp_key(searchterm):
     img_list = dwn.multidownload(urllist, td.name+'/'+searchterm, 'img_')
     num_images = len(img_list)
     dwn.multidownload(other_urls, td.name+'/not_'+searchterm, 'img_')
-    responseobj = {'dirname':td.name, 'num_images':num_images}
+    responseobj = {'dirname':td.name.split('\\')[-1], 'num_images':num_images}
     response = Response(json.dumps(responseobj), mimetype='application/json')
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+@app.route('/api/transform/<tdir>/<searchterm>')
+def transform_images(tdir, searchterm):
+    realtdir = tdbase + '\\' + tdir
+    img_dir = realtdir + '\\' + searchterm
+    img_list = os.listdir(img_dir)
+    for img in img_list:
+        synth.randomizer(img, img_dir, 3, 2)
+    with tarfile.open(realtdir + '\\' + 'training-set.tar.gz', 'w:gz') as tar:
+        tar.add(img_dir)
+        tar.add(realtdir + '\\not_' + searchterm)
+        return Response(tar,
+                        mimetype="application/gzip",
+                        headers={"Content-Disposition":"attachment;filename=training-set.tar.gz"})
 
 @app.route('/api/pretrained/apple/<url>')
 def run_pretrained_model(url):
